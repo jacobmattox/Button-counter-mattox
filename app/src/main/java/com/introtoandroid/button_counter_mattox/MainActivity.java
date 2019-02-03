8c009a986cd2110d6968d119e575db3b859e00fd
jacobmattox <jacob.mattox1017@gmail.com>
2019-02-03 09:43:29 -0500

    created the buttons in .xml and .java

--- app/src/main/java/com/introtoandroid/button_counter_mattox/MainActivity.java
+++ app/src/main/java/com/introtoandroid/button_counter_mattox/MainActivity.java
@@ -2,12 +2,24 @@ package com.introtoandroid.button_counter_mattox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counterVar = 0;
+    Button buttonPlusOne;
+    Button buttonMinusOne;
+    Button buttonReset;
+    TextView counterDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonPlusOne = (Button) findViewById(R.id.buttonPlusOne);
+        buttonMinusOne = (Button) findViewById(R.id.buttonMinusOne);
+        buttonReset = (Button) findViewById(R.id.buttonReset);
+        counterDisplay = (TextView) findViewById(R.id.textView);
     }
 }
