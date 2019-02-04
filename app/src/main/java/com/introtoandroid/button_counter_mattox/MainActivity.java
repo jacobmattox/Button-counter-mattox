a9936dbd2db131838ae6b78912f0da4f8f54a8d4
jacobmattox <jacob.mattox1017@gmail.com>
2019-02-04 16:16:51 -0500

    onClickListeners work, need to remove hardcoded counterVar

--- app/src/main/java/com/introtoandroid/button_counter_mattox/MainActivity.java
+++ app/src/main/java/com/introtoandroid/button_counter_mattox/MainActivity.java
@@ -2,10 +2,11 @@ package com.introtoandroid.button_counter_mattox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private int counterVar = 0;
     Button buttonPlusOne;
@@ -18,8 +19,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonPlusOne = (Button) findViewById(R.id.buttonPlusOne);
+        buttonPlusOne.setOnClickListener(this);
         buttonMinusOne = (Button) findViewById(R.id.buttonMinusOne);
+        buttonMinusOne.setOnClickListener(this);
         buttonReset = (Button) findViewById(R.id.buttonReset);
+        buttonReset.setOnClickListener(this);
         counterDisplay = (TextView) findViewById(R.id.textView);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.buttonPlusOne){
+            counterVar++;
+            counterDisplay.setText(Integer.toString(counterVar));
+        }
+        else if(v.getId() == R.id.buttonMinusOne){
+            counterVar = counterVar - 1;
+            counterDisplay.setText(Integer.toString(counterVar));
+        }
+        else if(v.getId() == R.id.buttonReset){
+            counterVar = 0;
+            counterDisplay.setText(Integer.toString(counterVar));
+        }
+
+    }
+
 }
