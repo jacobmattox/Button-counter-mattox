89469105dec7c33703ae222e68b4afb798d5d67b
jacobmattox <jacob.mattox1017@gmail.com>
2019-02-04 20:20:16 -0500

    fixed state saving and changed onClickListeners to be anon

--- app/src/main/java/com/introtoandroid/button_counter_mattox/MainActivity.java
+++ app/src/main/java/com/introtoandroid/button_counter_mattox/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
 
     private int counterVar;
     private Button buttonPlusOne;
@@ -18,34 +18,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         buttonPlusOne = (Button) findViewById(R.id.buttonPlusOne);
-        buttonPlusOne.setOnClickListener(this);
         buttonMinusOne = (Button) findViewById(R.id.buttonMinusOne);
-        buttonMinusOne.setOnClickListener(this);
         buttonReset = (Button) findViewById(R.id.buttonReset);
-        buttonReset.setOnClickListener(this);
-        counterDisplay = findViewById(R.id.textView);
-        counterVar = Integer.parseInt(counterDisplay.getText().toString());
+        counterDisplay = (TextView) findViewById(R.id.textView);
 
+        //can create setOnClickListener either way
+        //buttonPlusOne.setOnClickListener(this);
+        buttonPlusOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterVar++;
+                counterDisplay.setText(Integer.toString(counterVar));
+            }
+        });
 
+        //can create setOnClickListener either way
+        //buttonMinusOne.setOnClickListener(this);
+        buttonMinusOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterVar = counterVar - 1;
+                counterDisplay.setText(Integer.toString(counterVar));
+            }
+        });
+
+        //can create setOnClickListener either way
+        //buttonReset.setOnClickListener(this);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterVar = 0;
+                counterDisplay.setText(Integer.toString(counterVar));
+            }
+        });
+    }
+    //if implementing View.setOnClickListener then you need to define onClick as below
+//
+//    @Override
+//    public void onClick(View v) {
+//
+//        if(v.getId() == R.id.buttonPlusOne){
+//            counterVar++;
+//            counterDisplay.setText(Integer.toString(counterVar));
+//        }
+//        else if(v.getId() == R.id.buttonMinusOne){
+//            counterVar = counterVar - 1;
+//            counterDisplay.setText(Integer.toString(counterVar));
+//        }
+//        else if(v.getId() == R.id.buttonReset){
+//            counterVar = 0;
+//            counterDisplay.setText(Integer.toString(counterVar));
+//        }
+//
+//    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("counter", counterDisplay.getText().toString());
     }
 
     @Override
-    public void onClick(View v) {
-
-        if(v.getId() == R.id.buttonPlusOne){
-            counterVar++;
-            counterDisplay.setText(Integer.toString(counterVar));
-        }
-        else if(v.getId() == R.id.buttonMinusOne){
-            counterVar = counterVar - 1;
-            counterDisplay.setText(Integer.toString(counterVar));
-        }
-        else if(v.getId() == R.id.buttonReset){
-            counterVar = 0;
-            counterDisplay.setText(Integer.toString(counterVar));
-        }
-
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counterVar = Integer.parseInt(savedInstanceState.getString("counter"));
+        counterDisplay.setText(Integer.toString(counterVar));
     }
-
 }
